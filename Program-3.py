c48620192acbe0fc0db1362ace1cd8623be0d7b3
Dhanush12345-byte <dhanushvanam74@gmail.com>
2025-06-25 19:51:23 +0530

    Update Program-3.py

--- Program-3.py
+++ Program-3.py
@@ -1,1 +1,11 @@
+def conditional_odd_series(a):
+    if a % 2 == 0:
+        a -= 1
+    result = []
+    for i in range(a):
+        result.append(2 * i + 1)
+    return result
 
+# Example
+a = 6
+print("Output:", conditional_odd_series(a))  # Output: [1, 3, 5, 7, 9]
